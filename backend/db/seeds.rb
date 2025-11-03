2af5933ae4b56c7da79eca326e1fbd7c29bb0173
Nebiyu Elias <nebiyuelias1@gmail.com>
2025-11-03 09:25:34 +0800

    chore: seed database with ethiopic books

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -10,7 +10,7 @@
 #
 
 # Create or find tags
-tag_names = %w[አማርኛ ፍቅር ታሪክ ባህል የኢትዮጵያ_ልቦና]
+tag_names = %w[አማርኛ ፍቅር ታሪክ ባህል የኢትዮጵያ_ልቦና ፖለቲካ ህይወት ልቦና]
 tags = tag_names.index_with { |name| Tag.find_or_create_by!(name: name) }
 
 # Array of Ethiopian books (currently one)
@@ -31,6 +31,32 @@ books_data = [
     average_rating: 4.9,
     reviews_count: 0,
     tag_names: %w[አማርኛ ፍቅር ታሪክ ባህል]
+  },
+  {
+    title: "ኦሮማይ",
+    author: "በዓሉ ግርማ",
+    description: "የ1980ዎቹ በውስጥ የኢትዮጵያ የመንግስት ሥርዓትና የሞት ዘመን ውስጥ የአንድ የቴሌቪዥን ዘጋቢ ታሪክ። የተፈተነው የደርዳር ውጤት በግልፅነት ተገልጿል።",
+    published_at: Date.new(1983,1,1),
+    language: "am",
+    publisher: "Kuraz Publishing Agency",
+    isbn: "9789994400056",
+    cover_image: "https://typicalethiopian.com/wp-content/uploads/2022/03/Oromai_cover.png",
+    average_rating: 4.4,
+    reviews_count: 430,
+    tag_names: %w[አማርኛ ፖለቲካ ታሪክ]
+  },
+  {
+    title: "የተቆለፈበት ቁልፍ",
+    author: "ዶ/ር መህረት ደበበ",
+    description: "ልዩ ታሪክ በውጭ ተተመው የተነሳ የማኅበረሰብ እና የጭንቀት ጉዳዮችን የሚያሳይ። አብዛኛው በግልፅ አማርኛ ቋንቋ ተጻፈ።",
+    published_at: Date.new(1995,1,1),
+    language: "am",
+    publisher: "የኢትዮጵያ ዩኒቨርሲቲ ፕረስ",
+    isbn: "9789994400063",
+    cover_image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1437980703i/18007965.jpg",
+    average_rating: 4.0,
+    reviews_count: 290,
+    tag_names: %w[አማርኛ ህይወት ልቦና]
   }
 ]
 
